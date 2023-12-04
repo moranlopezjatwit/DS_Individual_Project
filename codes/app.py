e1ac6a9a5401fdadad1981e97bc018351a9d42c0
moranlopezjatwit <77841313+moranlopezjatwit@users.noreply.github.com>
2023-12-03 19:15:49 -0500

    Update app.py

--- codes/app.py
+++ codes/app.py
@@ -32,12 +32,49 @@ sb.barplot(x=val1.CustomerID.head(40),y=val1['Total Prices'].head(40),palette='h
 plt.xticks(rotation=90)
 plt.title("Maximum Price Customer Id")
 
-val2=data7.CustomerID.value_counts().sort_values(ascending=False).head(30)
-sb.barplot(x=val2.index,y=val2,palette='rainbow')
+val5=data7.Avg_Price
+sb.boxplot(val5,palette='hls')
+plt.title('Average Price Anaysis')
+plt.xlabel('Average Price')
+plt.xticks([0],['Central Tendency'])
+plt.ylabel('Prices')
 
-plt.xticks(rotation=90)
-plt.ylabel('No. of Purchases')
-plt.title('Top 30 Popular Customer ID with Purchase Count')
+sb.lineplot(y=new.Total_Spend,x=new.index,color='g')
+plt.xticks(new.index,mon,rotation=60)
+plt.title("Total Spend With Month")
+
+sb.lineplot(y=new.Online_Spend,x=new.index,color='pink')
+plt.xticks(new.index,mon,rotation=60)
+plt.title("Total Online Spend With Month")
+
+delivery=data7[['Delivery_Charges','Month']].groupby('Month').sum()
+sb.barplot(data=delivery,x=delivery.index,y='Delivery_Charges',palette='coolwarm')
+plt.xticks(delivery.index,mon,rotation=60)
+plt.title("Total Delivery Charges per Month")
+
+import matplotlib.pyplot as plt
+
+
+new1 = data7.groupby('Product_Category').size()
+
+
+fig, ax = plt.subplots()
+
+
+new1.sort_values().plot(kind='barh', color='skyblue', edgecolor='black', ax=ax)
+
+
+for index, value in enumerate(new1.sort_values()):
+    ax.text(value, index, f'{value/sum(new1)*100:.2f}%', va='center')
+
+# Set labels and title
+ax.set_xlabel('Frequency')
+ax.set_ylabel('Product Category')
+plt.title("Product Category Frequency Analysis")
+
+# Show the plot
+plt.show()
+
 
 
 
